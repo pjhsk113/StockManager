46ff31df9e5f4423a2e9cacffb4a71f06bfe2af4
pjhsk113 <pjhsk113@naver.com>
2021-01-28 23:00:40 +0900

    feat: Form Component 추가

---
+++ front/src/components/Form.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-form
+    inline>
+    <el-form-item>
+      <el-input/>
+    </el-form-item>
+    <el-form-item>
+      <el-input/>
+    </el-form-item>
+    <el-form-item>
+      <el-input/>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default class Form extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
