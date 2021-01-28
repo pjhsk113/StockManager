c83202ac32ba16c4ae8ecfc5b093aa9cf93f005d
pjhsk113 <pjhsk113@naver.com>
2021-01-28 23:02:36 +0900

    feat: App.vue 구성

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-row style="width: 100%">
+        <el-col>
+          <Title/>
+        </el-col>
+        <el-col>
+          asd
+        </el-col>
+      </el-row>
+    </el-container>
   </div>
 </template>
 
 <script>
+import { Component, Vue } from 'vue-property-decorator'
+import Title from '@/components/Title.vue'
 
+@Component({
+  components: {
+    Title
+  }
+})
+
+export default class App extends Vue {}
 </script>
 
 <style>
@@ -18,6 +33,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
