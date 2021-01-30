8879e00da603715c2ea6776708b9ab64b33f9dfc
pjhsk113 <pjhsk113@naver.com>
2021-01-30 19:04:28 +0900

    feat: project reset

--- front/src/api/enums.ts
+++
@@ -1,6 +0,0 @@
-export enum Sectors {
-  IT = 'IT',
-  FINANCE = '금융',
-  CAR = '자동차',
-  SEMICONDUCTOR = '반도체',
-}
--- front/src/api/types.ts
+++
@@ -1,9 +0,0 @@
-import {Sectors} from "@/api/enums";
-
-export interface IStockParams {
-  id: string | null,
-  name: string,
-  sectors: Sectors,
-  price: number,
-  quantity: number,
-}
