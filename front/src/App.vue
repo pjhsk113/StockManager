7e31f0c0d8b28ee3199520d4cdd6dc26b1ccc33e
pjhsk113 <pjhsk113@naver.com>
2021-01-30 20:11:08 +0900

    feat: add Element UI

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,37 +1,32 @@
 <template>
   <div id="app">
-    <el-container>
-      <el-row style="width: 100%">
-        <el-col>
-          <Title/>
-        </el-col>
-        <el-col>
-          asd
-        </el-col>
-      </el-row>
-    </el-container>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import { Component, Vue } from 'vue-property-decorator'
-import Title from '@/components/Title.vue'
-
-@Component({
-  components: {
-    Title
-  }
-})
-
-export default class App extends Vue {}
-</script>
-
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
