e6adb50670d3bc5e4effa26e7ab4a2d11448a726
pjhsk113 <pjhsk113@naver.com>
2021-02-04 21:42:55 +0900

    [feat] vue-google-chart test

--- front/src/components/stock/StockCalc.vue
+++ front/src/components/stock/StockCalc.vue
@@ -87,6 +87,12 @@
         label="비중"
         align="center"/>
     </el-table>
+    <GChart
+      :setting = "{ packages: ['treemap'] }"
+      type="TreeMapChart"
+      :data="chartData"
+      :options="chartOptions"
+    />
     <el-button
       v-if="tableVisible"
       @click="closeTable">
@@ -101,8 +107,13 @@ import { IStock } from './types'
 import { defualtStock } from './type'
 import { cloneDeep } from 'lodash'
 import {Sector} from "@/components/stock/enum";
+import { GChart } from 'vue-google-charts';
 
-@Component
+@Component({
+  components: {
+    GChart
+  }
+})
 export default class StockCalc extends Vue {
   @Prop() private msg!: string;
 
@@ -111,7 +122,52 @@ export default class StockCalc extends Vue {
   private tableVisible = false;
   private totalPrice = 0;
   private index: number = 0;
-
+  private chartData = [
+    ['Location', 'Parent', 'Market trade volume (size)', 'Market increase/decrease (color)'],
+    ['Global',    null,                 0,                               0],
+    ['America',   'Global',             0,                               0],
+    ['Europe',    'Global',             0,                               0],
+    ['Asia',      'Global',             0,                               0],
+    ['Australia', 'Global',             0,                               0],
+    ['Africa',    'Global',             0,                               0],
+    ['Brazil',    'America',            11,                              10],
+    ['USA',       'America',            52,                              31],
+    ['Mexico',    'America',            24,                              12],
+    ['Canada',    'America',            16,                              -23],
+    ['France',    'Europe',             42,                              -11],
+    ['Germany',   'Europe',             31,                              -2],
+    ['Sweden',    'Europe',             22,                              -13],
+    ['Italy',     'Europe',             17,                              4],
+    ['UK',        'Europe',             21,                              -5],
+    ['China',     'Asia',               36,                              4],
+    ['Japan',     'Asia',               20,                              -12],
+    ['India',     'Asia',               40,                              63],
+    ['Laos',      'Asia',               4,                               34],
+    ['Mongolia',  'Asia',               1,                               -5],
+    ['Israel',    'Asia',               12,                              24],
+    ['Iran',      'Asia',               18,                              13],
+    ['Pakistan',  'Asia',               11,                              -52],
+    ['Egypt',     'Africa',             21,                              0],
+    ['S. Africa', 'Africa',             30,                              43],
+    ['Sudan',     'Africa',             12,                              2],
+    ['Congo',     'Africa',             10,                              12],
+    ['Zaire',     'Africa',             8,                               10]
+  ];
+  private chartOptions = { // For v49 or before
+    highlightOnMouseOver: true,
+    maxDepth: 1,
+    maxPostDepth: 2,
+    minHighlightColor: '#8c6bb1',
+    midHighlightColor: '#9ebcda',
+    maxHighlightColor: '#edf8fb',
+    minColor: '#009688',
+    midColor: '#f7f7f7',
+    maxColor: '#ee8100',
+    headerHeight: 15,
+    showScale: true,
+    height: 500,
+    useWeightedAverageForAggregation: true
+  };
   private getResult () {
     this.tableVisible = true;
     this.getRatio()
