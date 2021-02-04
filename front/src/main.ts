9b0a6b2aa54da9622987e8841148728e472f196f
pjhsk113 <pjhsk113@naver.com>
2021-02-04 21:42:44 +0900

    [feat] vue-google-chart import

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,15 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
-import VueGraph from 'vue-graph'
-import ECharts from 'vue-echarts'
 import 'element-ui/lib/theme-chalk/index.css';
-import 'echarts/lib/chart/bar'
-import 'echarts/lib/component/tooltip'
-import 'echarts-gl'
+import VueGoogleCharts from 'vue-google-charts';
 
-Vue.use(VueGraph)
-Vue.use(ECharts)
+Vue.use(VueGoogleCharts)
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
