9166262926aecd2e7fb2c1f65e4287927ba71bee
pjhsk113 <pjhsk113@naver.com>
2021-02-05 22:04:54 +0900

    [feat] chart.js PieChart Test

---
+++ front/src/components/chart/PieChart.js
@@ -0,0 +1,34 @@
+import { Doughnut, mixins } from "vue-chartjs";
+
+export default {
+  extends: Doughnut,
+  props: ["data", "options"],
+  mounted() {
+    // this.chartData is created in the mixin.
+    // If you want to pass options please create a local options object
+    this.renderChart(this.data, {
+      borderWidth: "10px",
+      hoverBackgroundColor: "red",
+      hoverBorderWidth: "10px"
+    });
+  }
+};
+
+// import { Bar } from 'vue-chartjs'
+//
+// export default {
+//   extends: Bar,
+//   mounted () {
+//     // Overwriting base render method with actual data.
+//     this.renderChart({
+//       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+//       datasets: [
+//         {
+//           label: 'GitHub Commits',
+//           backgroundColor: '#f87979',
+//           data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+//         }
+//       ]
+//     })
+//   }
+// }
