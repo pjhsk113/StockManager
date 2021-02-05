18a2e7fd420e858561d19ed0c53d2b99dc34b6a7
pjhsk113 <pjhsk113@naver.com>
2021-02-05 22:06:33 +0900

    [feat] Chart Component 그려보기

--- front/src/components/stock/StockCalc.vue
+++ front/src/components/stock/StockCalc.vue
@@ -88,11 +88,13 @@
         align="center"/>
     </el-table>
     <GChart
-      :setting = "{ packages: ['treemap'] }"
-      type="TreeMapChart"
+      type="PieChart"
       :data="chartData"
       :options="chartOptions"
+      :event="chartEvents"
+      style="width: 900px; height: 500px;"
     />
+    <pie-chart :data="chartData1" :options="chartOptions1"></pie-chart>
     <el-button
       v-if="tableVisible"
       @click="closeTable">
@@ -108,10 +110,13 @@ import { defualtStock } from './type'
 import { cloneDeep } from 'lodash'
 import {Sector} from "@/components/stock/enum";
 import { GChart } from 'vue-google-charts';
+import PieChart from '@/components/chart/PieChart'
+
 
 @Component({
   components: {
-    GChart
+    GChart,
+    PieChart
   }
 })
 export default class StockCalc extends Vue {
@@ -123,51 +128,41 @@ export default class StockCalc extends Vue {
   private totalPrice = 0;
   private index: number = 0;
   private chartData = [
-    ['Location', 'Parent', 'Market trade volume (size)', 'Market increase/decrease (color)'],
-    ['Global',    null,                 0,                               0],
-    ['America',   'Global',             0,                               0],
-    ['Europe',    'Global',             0,                               0],
-    ['Asia',      'Global',             0,                               0],
-    ['Australia', 'Global',             0,                               0],
-    ['Africa',    'Global',             0,                               0],
-    ['Brazil',    'America',            11,                              10],
-    ['USA',       'America',            52,                              31],
-    ['Mexico',    'America',            24,                              12],
-    ['Canada',    'America',            16,                              -23],
-    ['France',    'Europe',             42,                              -11],
-    ['Germany',   'Europe',             31,                              -2],
-    ['Sweden',    'Europe',             22,                              -13],
-    ['Italy',     'Europe',             17,                              4],
-    ['UK',        'Europe',             21,                              -5],
-    ['China',     'Asia',               36,                              4],
-    ['Japan',     'Asia',               20,                              -12],
-    ['India',     'Asia',               40,                              63],
-    ['Laos',      'Asia',               4,                               34],
-    ['Mongolia',  'Asia',               1,                               -5],
-    ['Israel',    'Asia',               12,                              24],
-    ['Iran',      'Asia',               18,                              13],
-    ['Pakistan',  'Asia',               11,                              -52],
-    ['Egypt',     'Africa',             21,                              0],
-    ['S. Africa', 'Africa',             30,                              43],
-    ['Sudan',     'Africa',             12,                              2],
-    ['Congo',     'Africa',             10,                              12],
-    ['Zaire',     'Africa',             8,                               10]
+    ['Task', 'Hours per Day'],
+    ['Work',     11],
+    ['Eat',      2],
+    ['Commute',  2],
+    ['Watch TV', 2],
+    ['Sleep',    7]
   ];
-  private chartOptions = { // For v49 or before
-    highlightOnMouseOver: true,
-    maxDepth: 1,
-    maxPostDepth: 2,
-    minHighlightColor: '#8c6bb1',
-    midHighlightColor: '#9ebcda',
-    maxHighlightColor: '#edf8fb',
-    minColor: '#009688',
-    midColor: '#f7f7f7',
-    maxColor: '#ee8100',
-    headerHeight: 15,
-    showScale: true,
-    height: 500,
-    useWeightedAverageForAggregation: true
+  private chartOptions = {
+    chart: {
+      title: 'Company Performance',
+      subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+    },
+    is3D: true,
+  }
+  private chartData1 = {
+    hoverBackgroundColor: "red",
+    hoverBorderWidth: 10,
+    labels: ["Green", "Red", "Blue"],
+    datasets: [
+      {
+        label: "Data One",
+        backgroundColor: ["#41B883", "#E46651", "#00D8FF"],
+        data: [1, 10, 5]
+      }
+    ]
   };
+
+  private chartOptions1 = { hoverBorderWidth: 10 };
+
+  private chartEvents = {
+    click : () => {
+      this.closeTable();
+    }
+  }
+
   private getResult () {
     this.tableVisible = true;
     this.getRatio()
@@ -212,6 +207,7 @@ export default class StockCalc extends Vue {
 
   private closeTable() {
     this.tableVisible = false;
+    console.log(12);
   }
 }
 
--- front/src/main.ts
+++ front/src/main.ts
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-import VueGoogleCharts from 'vue-google-charts';
+import VueGoogleCharts from 'vue-google-charts'
 
 Vue.use(VueGoogleCharts)
 Vue.use(ElementUI)
